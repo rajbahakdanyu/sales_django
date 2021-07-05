f2b30f67b71093093f83f199065c4dd7fb6c34e8
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-05 19:50:17 +0545

    Fixed some bugs

--- reports_project/sales/admin.py
+++ reports_project/sales/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-# from .models import Position, Sale, CSV
+from .models import Position, Sale, CSV
 
-# admin.site.register(Position)
-# admin.site.register(Sale)
-# admin.site.register(CSV)
+admin.site.register(Position)
+admin.site.register(Sale)
+admin.site.register(CSV)
